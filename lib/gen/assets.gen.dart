26ce40c27f19c9a28325b20ad06cca1bda891852
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-01-22 17:08:55 +0900

    add: 画像を挿入

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,66 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class Assets {
+  Assets._();
+
+  static const AssetGenImage dart = AssetGenImage('assets/dart.png');
+  static const AssetGenImage flutter = AssetGenImage('assets/flutter.png');
+  static const AssetGenImage kotlin = AssetGenImage('assets/kotlin.png');
+  static const AssetGenImage swift = AssetGenImage('assets/swift.png');
+}
+
+class AssetGenImage extends AssetImage {
+  const AssetGenImage(String assetName) : super(assetName);
+
+  Image image({
+    Key? key,
+    ImageFrameBuilder? frameBuilder,
+    ImageLoadingBuilder? loadingBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? width,
+    double? height,
+    Color? color,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    FilterQuality filterQuality = FilterQuality.low,
+  }) {
+    return Image(
+      key: key,
+      image: this,
+      frameBuilder: frameBuilder,
+      loadingBuilder: loadingBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      filterQuality: filterQuality,
+    );
+  }
+
+  String get path => assetName;
+}
